fa7e55b6d99e0ce2378f239c1c0dc3ce40cefa05
Ruhan <ruhan21001@outlook.com>
2023-05-21 12:12:43 +0200

    loading

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,14 +3,20 @@
     import Card from "../../components/card.svelte";
 
     let profiles = [];
+    let isLoading = false;
 
     $: {
         profiles = $profile;
+        isLoading = profiles.length === 0;
     }
 </script>
 
 <div class="container">
     <h1 class="queue">SOLO QUEUE</h1>
+
+    {#if isLoading} <!-- Show loading spinner if isLoading is true -->
+        <i class="fa-solid fa-spinner fa-2xl spin"></i>
+    {:else}
     {#each profiles as prof, index}
         <Card
             style={index === 0 ? "border: 2px dashed #FFD700" : ""}
@@ -25,9 +31,21 @@
             profileIcon={prof.profileIconUrl}
         />
     {/each}
+    {/if}
 </div>
 
 <style>
+
+.spin {
+        animation: spin 1s infinite;
+    }@keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
     .queue {
         font-size: 2rem;
         margin-bottom: 2rem;
